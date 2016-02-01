12230e002c550292dc1414983176dd0e55501cf7
XIAZY <admin@niconiconi.rocks>
2016-02-01 19:48:03 +0800

    huge update to DM5

--- driveit.py
+++ driveit.py
@@ -1,24 +1,34 @@
 from base import SharedBase
 
 
-def main_loop(refer_box, is_volume=False):
-    total_parents = max(refer_box.keys())
-    if is_volume is True:
-        parent_str = 'Volume'
-    else:
-        parent_str = 'Chapter'
-    for parent in range(1, total_parents + 1):
-        if parent in refer_box.keys():
-            cid = refer_box[parent]
-            for page in range(1, website_object.get_page_info(cid) + 1):
-                link = website_object.get_image_link(cid, page)
-                try:
-                    website_object.down(comic_name, cid, link, parent, page, is_volume)
-                    print('%s %d page %d has been downloaded successfully' % (parent_str, parent, page))
-                except:
-                    print('Error occurred when downloading %s %d, Page %d.' % (parent_str, parent, page))
-        else:
-            print('Chapter %d cannot be found.' % parent)
+def main_loop(refer_box):
+    # total_parents = max(refer_box.keys())
+    # if is_volume is True:
+    #     parent_str = 'Volume'
+    # else:
+    #     parent_str = 'Chapter'
+    # for parent in range(1, total_parents + 1):
+    #     if parent in refer_box.keys():
+    #         cid = refer_box[parent]
+    #         for page in range(1, website_object.get_page_info(cid) + 1):
+    #             link = website_object.get_image_link(cid, page)
+    #             try:
+    #                 website_object.down(comic_name, cid, link, parent, page, is_volume)
+    #                 print('%s %d page %d has been downloaded successfully' % (parent_str, parent, page))
+    #             except:
+    #                 print('Error occurred when downloading %s %d, Page %d.' % (parent_str, parent, page))
+    #     else:
+    #         print('Chapter %d cannot be found.' % parent)
+    for ref_tuple in refer_box:
+        title, parent_link = ref_tuple
+        total_page = website_object.get_page_info(parent_link)
+        for page in range(1, total_page + 1):
+            link = website_object.get_image_link(parent_link, page)
+            try:
+                website_object.down(comic_name, parent_link, link, title, page)
+                print('%s page %d has been downloaded successfully' % (title, page))
+            except:
+                print('Error occurred when downloading %s, Page %d.' % (title, page))
 
 
 user_input_url = input('URL?\n')
@@ -29,9 +39,7 @@ elif base.get_site_name() is 'ck101':
     from sites import Ck101 as SiteClass
 website_object = SiteClass(user_input_url)
 comic_name = website_object.get_name()
-ref_box = website_object.get_chapter_info()
-print('%s, total %d chapters detected.' % (comic_name, max(ref_box.keys())))
+ref_box = website_object.get_parent_info()
+print('%s, total %d chapters detected.' % (comic_name, len(ref_box)))
 
 main_loop(ref_box)
-if website_object.is_volume() is True:
-    main_loop(website_object.get_volume_info(), True)
--- sites.py
+++ sites.py
@@ -2,6 +2,7 @@ from base import SharedBase
 import re
 import webbrowser
 import time
+from bs4 import BeautifulSoup
 
 
 class Ck101(SharedBase):
@@ -58,7 +59,7 @@ class Ck101(SharedBase):
 
 class DM5(SharedBase):
     def __init__(self, url):
-        self.general_formula = 'http://www.dm5.com/m%s/chapterfun.ashx?cid=%s&page=%s'
+        self.general_formula = 'http://www.dm5.com/%s/chapterfun.ashx?cid=%s&page=%s'
         self.flyleaf_url = url
         self.flyleaf_data = self.get_data(self.flyleaf_url).decode('utf-8')
         self.name = re.findall(r'\<title\>(.*?)\_', self.flyleaf_data)[0]
@@ -66,26 +67,33 @@ class DM5(SharedBase):
     def get_name(self):
         return self.name
 
-    def get_chapter_info(self):
-        ref_box = {}
-        chapter_match = re.compile(r'href\=\"\/m(\d+?)\/\"\stitle\=\"%s\s第(\d+?)话' % self.name)
-        for cid, chapter in chapter_match.findall(self.flyleaf_data):
-            if chapter not in ref_box.keys():
-                ref_box[int(chapter)] = int(cid)
+    def get_parent_info(self):
+        ref_box = []
+        # chapter_match = re.compile(r'href\=\"\/m(\d+?)\/\"\stitle\=\"%s\s第(\d+?)话' % self.name)
+        # for cid, chapter in chapter_match.findall(self.flyleaf_data):
+        #     if chapter not in ref_box.keys():
+        #         ref_box[int(chapter)] = int(cid)
+        page_soup = BeautifulSoup(self.flyleaf_data, 'html.parser')
+        page_soup_box = page_soup.findAll('ul', {'class': 'nr6 lan2', 'id': 'cbc_1'})
+        for border in page_soup_box:
+            for li in border.findAll('li'):
+                ref_link = li.a['href']
+                ref_title = li.a['title']
+                ref_box.insert(0, (ref_title, ref_link))
         return ref_box
 
-    def get_page_info(self, cid):
-        inner_page_data = self.get_data('http://www.dm5.com/m%d/' % cid).decode('utf-8')
-        pages = re.findall(r'\/m%s\-p(\d+)?\/\'\>第\d+?页' % cid, inner_page_data)
+    def get_page_info(self, parent_link):
+        inner_page_data = self.get_data('http://www.dm5.com%s' % parent_link).decode('utf-8')
+        pages = re.findall(r'%s\-p(\d+)?\/\'\>第\d+?页' % parent_link[:-1], inner_page_data)
         return int(pages[-1])
 
-    def get_image_link(self, cid, page):
+    def get_image_link(self, parent_link, page):
         node_script = ''
         while node_script is '':
-            node_script = self.get_data(self.general_formula % (cid, cid, page),
-                                        'http://www.dm5.com/m%d/' % cid).decode('utf-8')
+            node_script = self.get_data(self.general_formula % (parent_link, parent_link[2:-1], page),
+                                        'http://www.dm5.com%s' % parent_link).decode('utf-8')
             if node_script is '':
-                webbrowser.open_new('http://www.dm5.com/m%d/' % cid)
+                webbrowser.open_new('http://www.dm5.com%s' % parent_link)
                 time.sleep(3)
         refined_script = 'process.stdout.write(' + node_script.strip() + '+\'\\n\')'
         with open('towards_direct_link.js', 'w') as file:
@@ -95,7 +103,7 @@ class DM5(SharedBase):
         stdout = response.stdout.decode('utf-8')
         return stdout.split(',')[0]
 
-    def down(self, name, cid, link, parent, page, is_volume):
-        img_data = self.get_data(link, 'http://www.dm5.com/m%s/' % cid)
-        with open(self.get_path(name, parent, page, 'jpg'), 'wb+') as file:
+    def down(self, name, parent_link, link, parent_title, page):
+        img_data = self.get_data(link, 'http://www.dm5.com%s' % parent_link)
+        with open(self.get_path(name, parent_title, page, 'jpg'), 'wb+') as file:
             file.write(img_data)
