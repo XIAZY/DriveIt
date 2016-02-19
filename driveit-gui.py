ffc48e12c085383ce6f641cac56f35d1c07609d4
XIAZY <admin@niconiconi.rocks>
2016-02-20 02:12:23 +0800

    add chapter selection feature

--- driveit-gui.py
+++ driveit-gui.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import QThread, pyqtSignal, Qt
-from window import Ui_MainWindow
+from ui import Ui_MainWindow
 from base import SharedBase
 import sys
 
@@ -17,9 +17,16 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             self.lineEdit.setDisabled(True)
             self.user_input_url = self.lineEdit.text()
             self.pushButton.setDisabled(True)
+            self.checkBox.setDisabled(True)
+            self.spinBox.setDisabled(True)
+            self.label.setDisabled(True)
             self.base = SharedBase(self.user_input_url)
             self.site_name = self.base.get_site_name()
-            self.work = WorkingThread(self.site_name, self.user_input_url)
+            if self.checkBox.isChecked():
+                checkbox_value = self.spinBox.value()
+            else:
+                checkbox_value = False
+            self.work = WorkingThread(self.site_name, self.user_input_url, checkbox_value)
             self.work.status_report_signal.connect(self.status_receive_signal)
             self.work.progress_report_signal.connect(self.progress_receive_signal)
             self.work.start()
@@ -41,10 +48,11 @@ class WorkingThread(QThread):
     status_report_signal = pyqtSignal(str)
     progress_report_signal = pyqtSignal(float)
 
-    def __init__(self, site_name, url):
+    def __init__(self, site_name, url, checkbox_value):
         super(WorkingThread, self).__init__()
         self.site_name = site_name
         self.user_input_url = url
+        self.latest_limit = checkbox_value
 
     def run(self):
         if self.site_name == 'dm5':
@@ -55,31 +63,11 @@ class WorkingThread(QThread):
         self.comic_name = self.website_object.get_name()
         self.ref_box = self.website_object.get_parent_info()
         self.status_report_signal.emit('%s, total %d chapters detected.' % (self.comic_name, len(self.ref_box)))
+        if self.latest_limit is not False:
+            self.ref_box = self.ref_box[-self.latest_limit:]
         self.main_loop(self.ref_box)
 
-    def main_loop(self, refer_box, is_volume=False):
-        # total_parents = max(refer_box.keys())
-        # if is_volume is True:
-        #     parent_str = 'Volume'
-        # else:
-        #     parent_str = 'Chapter'
-        # for parent in range(1, total_parents + 1):
-        #     if parent in refer_box.keys():
-        #         cid = refer_box[parent]
-        #         for page in range(1, self.website_object.get_page_info(cid) + 1):
-        #             link = self.website_object.get_image_link(cid, page)
-        #             try:
-        #                 self.status_report_signal.emit(
-        #                         'Downloading %s %s %s' % (self.comic_name, parent_str, parent))
-        #                 progress = page / self.website_object.get_page_info(cid)
-        #                 self.website_object.down(self.comic_name, cid, link, parent, page, is_volume)
-        #                 self.progress_report_signal.emit(progress * 100)
-        #             except:
-        #                 self.status_report_signal.emit(
-        #                         'Error occurred when downloading %s %d, Page %d.' % (parent_str, parent, page))
-        #     else:
-        #         self.status_report_signal.emit('Chapter %d cannot be found.' % parent)
-        # self.status_report_signal.emit('All Done!')
+    def main_loop(self, refer_box):
         for ref_tuple in refer_box:
             title, parent_link = ref_tuple
             total_page = self.website_object.get_page_info(parent_link)
