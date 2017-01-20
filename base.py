603ca164538b0df1c6b3282031eb0b7ad542da52
Trim21 <niuziru21@gmail.com>
2017-01-21 03:48:30 +0800

    修改manhua.dmzj的url匹配规则

--- base.py
+++ base.py
@@ -16,7 +16,7 @@ class SharedBase(object):
             return 'ck101'
         if re.match(r'http://www.dmzj.com/info/.+?.html', self.url):
             return 'dmzj'
-        if re.match(r'http://manhua.dmzj.com/[a-z]+?/', self.url):
+        if re.match(r'http://manhua.dmzj.com/[a-z]+?', self.url):
             return 'manhua_dmzj'
         if re.match(r'http://g.e-hentai.org/g/\d+?/[a-zA-Z0-9]+?', self.url):
             return 'ehentai'
